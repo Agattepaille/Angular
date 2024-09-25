7e57a6a19f73dded87e04a6ffbd7b52b0bdeee0a
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-09-25 15:25:09 +0200

    article added

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.scss'
+})
+export class ArticleComponent {
+
+}
