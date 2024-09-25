15739a1e16825e6d41e4a7386bcb8e7c9161f707
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-09-25 17:08:14 +0200

    quete 3 en cours

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-article',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule, 
+    MatInputModule
   ],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+  article = {
+    title: 'Titre de l\'article',
+    author: 'John Doe',
+    content: 'Voici le contenu de l\'article.',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: ''
+  };
 
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
+
