ca91a988ec3bee6fc004d3414a8429cf0783c122
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-10-01 16:31:20 +0200

    quete 6 ok

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Message } from '../models/message.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
