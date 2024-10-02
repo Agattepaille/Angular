b09f20ac1d5f648a7815b79c4817eee30eb01363
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-10-02 12:12:48 +0200

    quete 7 ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArticlePageComponent } from './pages/article-page/article-page.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ContactFormComponent } from './pages/contact-form/contact-form.component';
+import { SignupFormComponent } from './pages/signup-form/signup-form.component';
 
 export const routes: Routes = [
-    { path: 'article/:id', component: ArticlePageComponent },  // Route dynamique pour afficher les détails d'un article
-    { path: '', component: HomePageComponent}, // Route pour la page d'accueil
-    { path: 'contact', component: ContactFormComponent },  // Route pour la page contact
+    { path: 'article/:id', component: ArticlePageComponent },  
+    { path: '', component: HomePageComponent},
+    { path: 'contact', component: ContactFormComponent },
+    { path: 'registration', component: SignupFormComponent }, 
+
     { path: '**', component: NotFoundComponent }
 
 ];
