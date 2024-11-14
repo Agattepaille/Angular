c28a6a77531a9e5d84be86155840cbef290d59ce
Agattepaille <amelie.gattepaille@hotmail.fr>
2024-11-14 11:00:39 +0100

    ok ish

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Article } from '../../models/article.models';
 import { HttpClient } from '@angular/common/http';
 import { ArticleComponent } from "../../components/article/article.component";
@@ -11,7 +11,6 @@ import { AsyncPipe } from '@angular/common';
   standalone: true,
   imports: [
     ArticleComponent,
-    AsyncPipe
   ],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss'
@@ -20,20 +19,29 @@ export class ArticlePageComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   articleId!: number;
 
-  article$!: Observable<Article>;
+  article!: Article;
+
+  // article$!: Observable<Article>;
+
+  articleSubscription!: Subscription;
+
 
   http = inject(HttpClient);
 
   getArticleById(id: number) {
-    this.article$ = this.http.get<Article>(`http://localhost:3000/articles/${id}`);
-    console.log(this.article$);
-    return this.article$;
+    this.http.get<Article>(`http://localhost:3000/articles/${id}`).subscribe(data => {
+      this.article = data;
+      return this.article;
+    });
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
+      return this.articleId;
     });
-    console.log(this.articleId);
+    this.getArticleById(this.articleId);
   }
+
+  
 }
